b41ef4c54d262eeb5fe0d0cfa66f3973979328b5
Szauek <adrian.szalkowskil@gmail.com>
2022-01-16 21:12:52 +0100

    Zaktualizowany plik models.py Dodany catalog oraz produkty

--- sklep_internetowy/sklep/apps.py
+++ sklep_internetowy/sklep/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
-
+from django.db import models
 
 class SklepConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sklep'
-#siemanko testowanko
-#sdasdasd
