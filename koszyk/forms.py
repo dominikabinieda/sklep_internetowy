38761c23a24fb529dc79356f9b8c5aa6f28a1376
Szauek <adrian.szalkowskil@gmail.com>
2022-01-17 00:01:53 +0100

    Stworzenie widoków do koszyka z dodawaniem produktow, usuwaniem oraz wyswietlaniem

--- koszyk/forms.py
+++ koszyk/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 PRODUCT_QUANTITY_CHOICES =[(i, str(i)) for i in range(1,21)]
 
-class KoszkAddProductForm(forms.Form):
+class KoszykAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
         coerce=int)
