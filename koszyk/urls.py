8adfacddb5c503ff92bc576980704987ac7a1ce3
Szauek <adrian.szalkowskil@gmail.com>
2022-01-17 00:14:38 +0100

    Aktualizacja adresów URL w koszyk.url oraz w głównym url

---
+++ koszyk/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+
+app_name = 'koszyk'
+
+urlpatterns = [
+    path('', views.koszyk_wyswietl, name ='koszyk-wyswietl'),
+    path('add/<int:product_id>/',
+         views.koszyk_add, name='koszyk_dodaj'),
+    path('remove/<int:product_id>/',
+         views.koszyk_remove, name = 'koszyk_remove'),
+]
