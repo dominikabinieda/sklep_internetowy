38908a653dc1f73b6f700e79a0c5478e980159ed
Dominika Binieda <binieda.dominika@gmail.com>
2022-01-16 21:33:24 +0100

    Stworzenie urls-ów dla katalogu produktów (lista produktów bez żadnych parametrów i widok ze względu na kategorię produktu); settings.py - poprawa nazwy w INSTALLED_APPS.

--- sklep_internetowy/urls.py
+++ sklep_internetowy/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import include, path
 from rest_framework import routers
 from sklep_internetowy.sklep import views
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -23,7 +25,15 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
+app_name = 'sklep'
+
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    #path('admin/', admin.site.urls),
+    #path('', include('shop.urls', namespace='shop')),
 ]
