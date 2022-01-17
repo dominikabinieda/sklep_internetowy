fa77eb2776a37d1dbba138b4d61b0e1810c4760c
Dominika Binieda <binieda.dominika@gmail.com>
2022-01-17 19:00:57 +0100

    Wstępnie opracowany CSS oraz HTML dla Koszyka.

--- zamowienia/urls.py
+++ zamowienia/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
+
 app_name = 'zamowienia'
 urlpatterns = [
- path('create/', views.order_create, name='order_create'),
+    path('create/', views.order_create, name='order_create'),
 ]
