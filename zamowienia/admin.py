94d6c45246c183d50e100b848c803834aa408d3e
Dominika Binieda <binieda.dominika@gmail.com>
2022-01-17 20:14:51 +0100

    !! Nie pobierajcie tego vol 2

--- zamowienia/admin.py
+++ zamowienia/admin.py
@@ -6,13 +6,10 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
-
+@admin.register(Zamowienia)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'imie', 'nazwisko', 'email',
                     'adres', 'kod_pocztowy', 'miasto', 'platnosc',
                     'utworzono', 'zaktualizowano']
     list_filter = ['platnosc', 'utworzono', 'zaktualizowano']
     inlines = [OrderItemInline]
-
-
-admin.site.register(Zamowienia, OrderAdmin)
